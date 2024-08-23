27a0e6eaa83e94f9047885e602755340294ae5b1
RobsonFe <robson_20ferreira@hotmail.com>
2024-08-23 14:35:44 -0300

    documentation by swagger

--- core/urls.py
+++ core/urls.py
@@ -16,8 +16,29 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API de Finanças integrada com Notion",
+        default_version='v1',
+        description="API feita para gerenciar finanças no AppWeb e sincronizar com Notion.",
+        contact=openapi.Contact(email="robson-ferreiradasilva@hotmail.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('financas.urls'))
+    path('api/v1/', include('financas.urls')),
+    path('docs/', schema_view.with_ui('swagger',
+                                      cache_timeout=0), name='schema-swagger-ui'),
+    path('api/api.json', schema_view.without_ui(cache_timeout=0),
+         name='schema-swagger-json'),
+    path('redoc/', schema_view.with_ui('redoc',
+         cache_timeout=0), name='schema-redoc'),
 ]
--- financas/urls.py
+++ financas/urls.py
@@ -3,15 +3,17 @@ from django.urls import path
 from financas.views.views import FinancasFindByIdView, FinancasCreateView, FinancasDeleteView, FinancasFindyByNotionIdView, FinancasListView, FinancasUpdateView
 
 urlpatterns = [
-    path('financas/create/',  FinancasCreateView.as_view(), name='create'),
-    path('financas/list',   FinancasListView.as_view(), name='list'),
+    path('financas/create/',  FinancasCreateView.as_view(), name='Criar  Finanças'),
+
+    path('financas/list',   FinancasListView.as_view(),
+         name='Listas todas as Finanças'),
     path('financas/findby/<uuid:pk>',
-         FinancasFindByIdView.as_view(),  name='findby_id'),
+         FinancasFindByIdView.as_view(),  name='Buscar dados pelo ID'),
     path('financas/findid/<str:notion_page_id>/',
-         FinancasFindyByNotionIdView.as_view(),  name='findby_notion_id'),
+         FinancasFindyByNotionIdView.as_view(),  name='Buscar Dados pelo ID do Notion'),
     path('financas/update/<uuid:pk>',
-         FinancasUpdateView.as_view(), name='update'),
+         FinancasUpdateView.as_view(), name='Atualizar Dados'),
     path('financas/delete/<uuid:pk>',
-         FinancasDeleteView.as_view(), name='delete')
+         FinancasDeleteView.as_view(), name='Deletar Dados')
 
 ]
